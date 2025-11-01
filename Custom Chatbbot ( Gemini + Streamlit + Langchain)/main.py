54393be2cba32ba0d899be7b428b00dd3ee0c9d1
Lakeshprabhu <f20220298@dubai.bits-pilani.ac.in>
2025-11-01 21:54:32 +0400

    commit message

--- Custom Chatbbot ( Gemini + Streamlit + Langchain)/main.py
+++ Custom Chatbbot ( Gemini + Streamlit + Langchain)/main.py
@@ -1,6 +1,21 @@
 import streamlit as st
-
+from PyPDF2 import PdfReader
 
 st.header("My first Chatbot")
 
 with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions",type="pdf")
+
+
+
+#tHIS IS THE PART OF CODE FOR TEXT EXTRACTION
+if file is not None:
+    pdf_read = PdfReader(file)
+    text = ""
+    for page in pdf_read.pages:
+        text+=page.extract_text()
+
+
+
+#TO BREAK INTO THE CHUNKS
